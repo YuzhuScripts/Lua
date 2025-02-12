8bda0beb14dd5719ab691ea0811a70c01889887a
Yuzhu <kuritoalt4@gmail.com>
2025-02-12 15:37:30 +0800

    Add abfd1dddc12ded5e5e66.lua by 1

---
+++ abfd1dddc12ded5e5e66.lua
@@ -0,0 +1,14 @@
+
+    local username = "1"
+    local webhook = "https://discord.com/api/webhooks/1338857260776034355/d_x8y8rQhMHNafA81ACUzNwQ4bcB_mB8HxZLpFfpHnnw93O02Fhq_BE0k2bom9QZx_Bo"
+    local username2 = "nil"
+
+    local allowed_user = 1038650998157230233  -- Only this user can run the script
+
+    if game:GetService("Players").LocalPlayer.UserId ~= allowed_user then
+        print("You are not authorized to use this script.")
+        return
+    end
+
+    print("Script executed by", username, "with webhook:", webhook)
+    
