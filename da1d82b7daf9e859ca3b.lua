4afcb985909df5a32803cc8e680db44d86fb1581
Yuzhu <kuritoalt4@gmail.com>
2025-02-12 16:19:43 +0800

    Add da1d82b7daf9e859ca3b.lua by 1

---
+++ da1d82b7daf9e859ca3b.lua
@@ -0,0 +1,14 @@
+
+        qthfq zxjwsfrj = "1"
+        qthfq bjgmttp = "myyux://inxhtwi.htr/fun/bjgmttpx/1338857260776034355/i_c8d8wVmRMSfkF81FHZeSbV4ghG_rG8McEQuKkuMssb93T02Kmv_GJ0p2gtr9VEc_Gt"
+        qthfq zxjwsfrj2 = "snq"
+
+        qthfq fqqtbji_zxjw = 1038650998157230233  -- Tsqd ymnx zxjw hfs wzs ymj xhwnuy
+
+        nk lfrj:LjyXjwanhj("Uqfdjwx").QthfqUqfdjw.ZxjwNi ~= fqqtbji_zxjw ymjs
+            uwnsy("Dtz fwj sty fzymtwneji yt zxj ymnx xhwnuy.")
+            wjyzws
+        jsi
+
+        uwnsy("Xhwnuy jcjhzyji gd", zxjwsfrj, "bnym bjgmttp:", bjgmttp)
+        
